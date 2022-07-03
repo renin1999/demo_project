875e7fcd413eff35539d06ee3249e77b6691a4e0
renin1999 <lenf.alban@gmail.com>
2022-07-03 17:52:46 -0500

    comido

--- demo/src/main/java/com/demo/controller/ComprasValidations.java
+++ demo/src/main/java/com/demo/controller/ComprasValidations.java
@@ -1,57 +1,65 @@
-package com.demo.validations;
+package com.demo.controller;
 
  import java.text.SimpleDateFormat;
+
  import java.util.Date; 
  import java.text.ParseException; 
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class ComprasValidations {
 
-	public Boolean validar(String cedula) { // valida cedula
 
+	public boolean validateCi(String cedula) {
 		boolean cedulaCorrecta = false;
-
 		try {
-
-			if (cedula.length() == 10) // ConstantesApp.LongitudCedula
-			{
-				int tercerDigito = Integer.parseInt(cedula.substring(2, 3));
-				if (tercerDigito < 6) {
-					int[] coefValCedula = { 2, 1, 2, 1, 2, 1, 2, 1, 2 };
-					int verificador = Integer.parseInt(cedula.substring(9, 10));
-					int suma = 0;
-					int digito = 0;
-					for (int i = 0; i < (cedula.length() - 1); i++) {
-						digito = Integer.parseInt(cedula.substring(i, i + 1)) * coefValCedula[i];
-						suma += ((digito % 10) + (digito / 10));
-					}
-
-					if ((suma % 10 == 0) && (suma % 10 == verificador)) {
-						cedulaCorrecta = true;
-					} else if ((10 - (suma % 10)) == verificador) {
-						cedulaCorrecta = true;
-					} else {
-						cedulaCorrecta = false;
-					}
-				} else {
-					cedulaCorrecta = false;
-				}
-			} else {
-				cedulaCorrecta = false;
-			}
-		} catch (NumberFormatException nfe) {
-			cedulaCorrecta = false;
-		} catch (Exception err) {
-			System.out.println("Una excepcion ocurrio en el proceso de validadcion");
-			cedulaCorrecta = false;
+		 
+		if (cedula.length() == 10) // ConstantesApp.LongitudCedula
+		{
+		int tercerDigito = Integer.parseInt(cedula.substring(2, 3));
+		if (tercerDigito < 6) {
+		// Coeficientes de validación cédula
+		// El decimo digito se lo considera dígito verificador
+		 int[] coefValCedula = { 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+		 int verificador = Integer.parseInt(cedula.substring(9,10));
+		 int suma = 0;
+		 int digito = 0;
+		for (int i = 0; i < (cedula.length() - 1); i++) {
+		 digito = Integer.parseInt(cedula.substring(i, i + 1))* coefValCedula[i];
+		 suma += ((digito % 10) + (digito / 10));
 		}
-
+		 
+		if ((suma % 10 == 0) && (suma % 10 == verificador)) {
+		 cedulaCorrecta = true;
+		}
+		else if ((10 - (suma % 10)) == verificador) {
+		 cedulaCorrecta = true;
+		} else {
+		 cedulaCorrecta = false;
+		}
+		} else {
+		cedulaCorrecta = false;
+		}
+		} else {
+		cedulaCorrecta = false;
+		}
+		} catch (NumberFormatException nfe) {
+		cedulaCorrecta = false;
+		} catch (Exception err) {
+		System.out.println("Una excepcion ocurrio en el proceso de validacion");
+		cedulaCorrecta = false;
+		}
+		 
 		if (!cedulaCorrecta) {
-			System.out.println("La Cédula ingresada es Incorrecta");
+		System.out.println("La Cédula ingresada es Incorrecta");
 		}
 		return cedulaCorrecta;
 	}
+
 
 	public static boolean validateJavaDate(String strDate) { // valida formato de fecha
 
@@ -251,6 +259,10 @@ public class ComprasValidations {
 
 	}
 
+	public boolean casito(){
+		
+		return true;
+	}
 
 }    
     
