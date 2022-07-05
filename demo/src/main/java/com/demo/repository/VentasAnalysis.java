2053a2a45e61e88b7c3decbdc8e2cc00bb46385d
renin1999 <lenf.alban@gmail.com>
2022-07-05 09:54:01 -0500

    comido

--- demo/src/main/java/com/demo/repository/VentasAnalysis.java
+++ demo/src/main/java/com/demo/repository/VentasAnalysis.java
@@ -5,35 +5,50 @@ import java.util.List;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.demo.model.Venta;
 
 public interface VentasAnalysis extends JpaRepository<Venta, Integer> {
-	
-	@Query(value= "select top 1 total_ventas,[idventas],[cantidad],[codventas],[fechaventa] from ventas  where year(fechaventa)=?1"
+	@Modifying 
+	@Transactional
+	@Query(value= "select top 1 totalventas,[idventas],[cantidad],[codventas],[fechaventa] "
+			+ "from ventas  where year(fechaventa)=?1"
 			, nativeQuery = true)
 	ArrayList findByTop1Ventas(int anio);
 	
+	@Modifying 
+	@Transactional
 	@Query(value= "SELECT avg (totalventas) from ventas", nativeQuery = true)
 	ArrayList findByAVGventas();
-
+	
+	@Modifying 
+	@Transactional
 	@Query(value= "\r\n"
 			+ "SELECT SUM (totalventas) from ventas", nativeQuery = true)
 	ArrayList findByGananciasTotales();
 	
+	@Modifying 
+	@Transactional
 	@Query(value= "\r\n"
 			+ "SELECT SUM (totalventas) from ventas\r\n"
 			+ "WHERE NOT ( year(fechaventa) = ?1)", nativeQuery = true)
 	ArrayList findByGananciasporAnio(int anio);
+	
+	@Modifying 
+	@Transactional
 	@Query(value= "\r\n"
 			+ "SELECT SUM (totalventas) from ventas\r\n"
-			+ "WHERE NOT ( Month(fecha_venta) > ?1 OR Month(fechaventa) < ?2)", nativeQuery = true)
+			+ "WHERE NOT ( Month(fechaventa) > ?1 OR Month(fechaventa) < ?2)", nativeQuery = true)
 	ArrayList findByGananciaspormeses(int hasta, int desde);
 	
+	@Modifying 
+	@Transactional
 	@Query(value= "select top 10 art.codarticulo, max (vent.totalventas) \r\n"
 			+ "	from ventas as vent join articulo as art on vent.idarticulo=art.idarticulo\r\n"
-			+ "	group by art.cod_articulo,vent.total_ventas", nativeQuery = true)
+			+ "	group by art.cod_articulo,vent.totalventas", nativeQuery = true)
 	ArrayList findBytop10articulosmasvendidos();
 
 }
